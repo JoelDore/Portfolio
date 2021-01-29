a420227897c9d805de58aa4388aa2df45df1bda3
JES20 <dore.joel.dore@gmail.com>
2021-01-29 11:44:41 -0500

    fix container width, add bg gradients

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,12 +1,20 @@
 $(document).ready(() => {
 
     const btnToTop = $("#scrollToTop")
+    const navLinks = $(".nav-link")
 
     $(window).scroll(() => {
+        // Within top half of intro section
         if ($(this).scrollTop() < window.innerHeight / 2) {
+            // hide button 
             btnToTop.addClass("d-none")
+            // reset nav links
+            navLinks.removeClass("text-light")
         } else {
+            // show button
             btnToTop.removeClass("d-none")
+            // lighten nav links
+            navLinks.addClass("text-light")
         }
     })
 })
