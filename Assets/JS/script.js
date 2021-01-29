cf9f2c39d2420b61f30623ffb795ac6d071a9854
JES20 <dore.joel.dore@gmail.com>
2021-01-29 15:40:20 -0500

    Layout 'Story' section

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,19 +2,22 @@ $(document).ready(() => {
 
     const btnToTop = $("#scrollToTop")
     const navLinks = $(".nav-link")
+    const navbar = $(".navbar")
 
     $(window).scroll(() => {
         // Within top half of intro section
         if ($(this).scrollTop() < window.innerHeight / 2) {
             // hide button 
             btnToTop.addClass("d-none")
-            // reset nav links
+            // reset navbar
             navLinks.removeClass("text-light")
+            navbar.removeClass("navbar-dark")
         } else {
             // show button
             btnToTop.removeClass("d-none")
-            // lighten nav links
+            // lighten navbar
             navLinks.addClass("text-light")
+            navbar.addClass("navbar-dark")
         }
     })
 })
